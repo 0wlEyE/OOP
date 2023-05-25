dd12fed43e393b13d317f73f6258f6f3c6df8b2e
F4n74zY <65070187@kmitl.ac.th>
2023-05-25 18:02:09 +0700

    Add files via upload

--- FUCKING NEW PROJECT/Table.java
+++ FUCKING NEW PROJECT/Table.java
@@ -1,16 +1,44 @@
-import javax.swing.JInternalFrame;
-import javax.swing.JLabel;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.*;
 
 public class Table extends JInternalFrame{
-    JLabel lab;
-    public Table(){
+    private JPanel panel;
+    private JTable table;
+    private JScrollPane scrollPane;
 
+    public Table(){
         new JInternalFrame();
+
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        scrollPane = new JScrollPane();
+        scrollPane.setBounds(0, 0, 350, 500);
+        add(scrollPane);
+
+        table = new JTable();
+        scrollPane.setViewportView(table);
+
+        DefaultTableModel model = (DefaultTableModel)table.getModel();
+        model.addColumn("Date"); 
+        model.addColumn("Information");
+        model.addColumn("Amount");
+        for(int i=0;i <= 144; i++) {
+            model.addRow(new Object[0]);
+            if (i%2 == 0){
+                model.setValueAt("12/01/2023", i, 0);
+                model.setValueAt("Eating", i, 1);
+                model.setValueAt("12000", i, 2);
+            }else{
+                model.setValueAt("21/10/2032", i, 0);
+                model.setValueAt("Nothing", i, 1);
+                model.setValueAt("21000", i, 2);
+            }
+        }
+
+        add(panel);
         setBounds(450, 20,350 , 500);
         setVisible(true);
-
-        lab = new JLabel("aaa");
-        this.add(lab);
-
     }
 }
